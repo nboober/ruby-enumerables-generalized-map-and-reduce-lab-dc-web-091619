7d6709af01f43f128912f2c700c116e1d2eb92c1
Nicholas Boober <nickboober92@gmail.com>
2019-09-10 15:50:16 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,51 +1,15 @@
 def map(array)
-
-  array.map{|i| i * -1}
-
-end
-
-
-
-
-
-def mymap(array)
   
-  array.map{|i| i}
-  
-end
+  new  = []
+  i = 0
 
-
-def map_to_double(source_array)
+  while i < array.length
   
-  source_array.map{|i| i*2}
+    new.push(yield(array[i]))
+    i+=1
+    
+  end
   
-end
-
-
-def map_to_square(source_array)
-  
-  source_array.map{|i| i**2}
-  
-end
-
-def reduce_to_total(source_array, starting_point = 0)
-  
-  
-  source_array.reduce(starting_point){|sum, num| sum + num}
-  
-  
-end
-
-
-def reduce_to_all_true(source_array)
-  
-source_array.all?
-
-end
-
-
-def reduce_to_any_true(source_array)
-  
-source_array.any?
+  new
   
 end
